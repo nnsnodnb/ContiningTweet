8b5f6698a2ca8ebc53448e1fbf7eccef289e7e2b
nnsnodnb <ahr63_gej@me.com>
2016-09-19 11:36:41 +0900

    Convert Swift3

---
+++ Pods/RevealingSplashView/RevealingSplashView/SplashAnimationType.swift
@@ -0,0 +1,25 @@
+//
+//  AnimationType.swift
+//  RevealingSplashView
+//
+//  Created by Chris Jimenez on 2/25/16.
+//  Copyright © 2016 Chris Jimenez. All rights reserved.
+//
+
+import Foundation
+
+/**
+ The types of animation supported
+ 
+ - Twitter: The default animation type is the Twitter App animation
+ */
+public enum SplashAnimationType: String{
+    
+    case Twitter
+    case RotateOut
+    case WoobleAndZoomOut
+    case SwingAndZoomOut
+    case PopAndZoomOut
+    case SqueezeAndZoomOut
+    
+}
