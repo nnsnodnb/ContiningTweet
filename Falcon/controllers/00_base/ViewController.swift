afd9e79fd3be9c45675133986e64e4a67085160e
nnsnodnb <ahr63_gej@me.com>
2016-10-17 22:08:29 +0900

    Initial commit

---
+++ Falcon/controllers/00_base/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Falcon
+//
+//  Created by Oka Yuya on 2016/10/17.
+//  Copyright © 2016年 nnsnodnb.moe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'Falcon' do
+    pod 'Firebase'
+    pod 'Firebase/Core'
+    pod 'Firebase/AdMob'
+end
+
+target 'FalconTests' do
+
+end
+
+target 'FalconUITests' do
+
+end
+
